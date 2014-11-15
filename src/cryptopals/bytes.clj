e645d5e31109b0716f57f1407527d24e2f29104e
Daniel Compton <desk@danielcompton.net>
2014-11-15 19:12:44 +1300

    Add leiningen project

---
+++ src/cryptopals/bytes.clj
@@ -0,0 +1,2 @@
+(ns cryptopals.bytes)
+
---
+++ test/cryptopals/set1.clj
@@ -0,0 +1,7 @@
+(ns cryptopals.set1
+  (:require [clojure.test :refer :all]
+            [cryptopals.bytes :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
